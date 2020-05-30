8659335be071eda1b86bf154124ab93836ade109
andrew <andrewkirsfelds@gmail.com>
2020-05-30 21:10:09 +0100

    adding quotes

--- app.py
+++ app.py
@@ -10,11 +10,19 @@ quotes = [
 	"This is a dude who, 700 years ago, totally ravaged China, and who, we were told, 2 hours ago, totally ravaged Oshman’s Sporting Goods.",
 	"Be excellent to each other!",
 	"It seems to me the only thing you’ve learned is that Caesar is a 'salad dressing dude'.",
-	"'All we are is dust in the wind,'' dude.",
+	"All we are is dust in the wind, dude.",
 	"Strange things are afoot at the Circle-K.",
 	"We're in danger of flunking most heinously tomorrow, Ted.",
 	"Party on dudes!!",
 	"Excellent!",
+        "Iron Maiden? Excellent!",
+        "Who was Joan of Arc? Noah's wife?!",
+        "Shut up, Ted",
+        "You know how to play, Rufus?",
+        "I play a little...",
+        "This is.. Dave Beeth-Oven... Maxine of Arc... Herman the Kid..., Bob Genghis Khan... So-crates Johnson... Dennis Freud. And uh... Abraham Lincoln...",
+        "San Dimas High School football rules!",
+        "That is why we need Eddie Van Halen!",
 	"This has been a most excellent adventure."
 ]
 
